a444d7ef6589f801dc7cfe06ce678fb0dd383465
Alex Jose <turmawb@gmail.com>
2024-04-02 22:05:47 -0300

    Criacao do CARD e sua funcionalidades: Listagem (fixa)

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalComponent } from '../modal/modal.component';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.css']
 })
-export class CardComponent implements OnInit {
+export class CardComponent {
+  cards = [
+    { title: 'Conexão Itau', description: 'Descrição do Card 1' },
+    { title: 'tecnologia no PDV', description: 'Descrição do Card 2' },
+    { title: 'beneficios Ticket', description: 'Descrição do Card 3' },
+    { title: 'Omier sistema de gestão', description: 'Descrição do Card 1' },
+    { title: 'gestçao de RH ', description: 'Descrição do Card 2' },
+    { title: 'conteúdos', description: 'Descrição do Card 3' },
+    { title: 'noticias', description: 'Descrição do Card 3' }
+  ];
 
-  constructor() { }
+  constructor(private modalService: NgbModal) {}
 
-  ngOnInit(): void {
+  openModal(card: any) {
+    const modalRef = this.modalService.open(ModalComponent);
+    modalRef.componentInstance.card = card;
   }
-
 }
